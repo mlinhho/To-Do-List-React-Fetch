fef82eefc20627f15103395b1ea7a53a10b9e1f2
mlinhho <mailinhtranho8@gmail.com>
2023-03-29 21:03:08 +0000

    completed all but delete functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  function _handleKeyDown(e) {
+    if (e.key === "Enter") {
+      setTodos(toDos.concat([inputValue]));
+      setInputValue("");
+    }
+  }
+  const [inputValue, setInputValue] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const [isShown, setIsShown] = useState(false);
+  return (
+    <div className="text-center">
+      <h1 className="text-center">TODOS</h1>
+      <div className="notePad">
+        <ul className="w-100">
+          <li>
+            <input
+              type="text"
+              id="addToDo"
+              placeholder="Add task"
+              onKeyDown={_handleKeyDown}
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+            />
+          </li>
+
+          {toDos.map((item, index) => (
+            <li
+              onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}
+              className="w-100 d-flex"
+            >
+              <p className="w-auto">{item}</p>
+              {isShown === true ? (
+                <i className="fa-solid fa-xmark ms-auto"></i>
+                ) : (
+                ""
+              )}
+            </li>
+          ))}
+        </ul>
+
+        <div> {toDos.length} item left</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
