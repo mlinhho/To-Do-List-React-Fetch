98e26c25fab5253a32cf4ec44622d9dbd15f1d0e
mlinhho <mailinhtranho8@gmail.com>
2023-04-03 23:20:13 +0000

    completed project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,15 +5,40 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   
-  const [todos, setTodos] = useState([{
-    label: "anyString",
-    done: false,
-  }, {
-    label: "anyString",
-    done: true,
-  }]);
+  const [todos, setTodos] = useState([]);
+
+  const [mounted, setMounted] = useState(false);
+
+  function getToDosFromAPI(){
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/mlinhho', {
+      method: "GET",
+      headers:{
+        "Content-Type":"application/json"
+      },
+    })
+    .then(response => {
+        console.log(response.ok); // will be true if the response is successfull
+        console.log(response.status); // the status code = 200 or code = 400 etc.
+        if (response.ok!=true) throw new Error ("response is not ok", response.status);
+        return response.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(toDosFromAPI => {
+        //here is were your code should start after the fetch finishes
+        setTodos(toDosFromAPI);
+        console.log(toDosFromAPI); //this will print on the console the exact object received from the server
+    })
+    .catch(error => {
+        //error handling
+        console.log(error);
+    })
+    .finally(() => {
+      setMounted(true)
+    })
+    ;
+  }
   
   function updateToDosOnAPI() {
+    if (todos.length == 0) return deleteUserToDosOnAPI();
     fetch('https://assets.breatheco.de/apis/fake/todos/user/mlinhho', {
       method: "PUT",
       body: JSON.stringify([
@@ -32,23 +57,27 @@ const Home = () => {
     .catch(error => {
       //error handling
       console.log(error);
-    });
+      getToDosFromAPI();
+    })
   }
   
-  function getToDosFromAPI(){
+  function deleteUserToDosOnAPI() {
     fetch('https://assets.breatheco.de/apis/fake/todos/user/mlinhho', {
-      method: "GET",
+      method: "DELETE",
+      headers:{
+        "Content-Type":"application/json"
+      },
     })
     .then(response => {
-        console.log(response.ok); // will be true if the response is successfull
-        console.log(response.status); // the status code = 200 or code = 400 etc.
-        if (response.ok!=true) throw new Error ("response is not ok", response.status);
-        return response.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+      console.log(response.ok); // will be true if the response is successfull
+      console.log(response.status); // the status code = 200 or code = 400 etc.
+      if (response.ok!=true) throw new Error ("response is not ok", response.status);
+      return response.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
     })
     .then(toDosFromAPI => {
-        //here is were your code should start after the fetch finishes
-        setTodos(toDosFromAPI);
-        console.log(toDosFromAPI); //this will print on the console the exact object received from the server
+      //here is were your code should start after the fetch finishes
+     
+      console.log(toDosFromAPI); //this will print on the console the exact object received from the server
     })
     .catch(error => {
         //error handling
@@ -61,9 +90,11 @@ const Home = () => {
   },[]);
 
   useEffect(() => {
+    if (!mounted) return
     updateToDosOnAPI()
     console.log(todos)
-  },[todos]);
+  },[todos,mounted]);
+
 
   return (
     <div className="container">
